a73cc4d15817edd41cb3e46b6f23f4a95bf1a91f
Sakhaya <96055547+Sakhaya@users.noreply.github.com>
2021-12-29 01:31:12 +1000

    Add files via upload

---
+++ OOP lab Nasledovanie.cpp
@@ -0,0 +1,44 @@
+﻿// OOP lab Nasledovanie.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class School {
+public:
+    void Forma(){
+
+    }
+};
+
+class Teacher : public School {
+public :
+    void Forma() {
+        cout << "Delovaya forma odezhdu.";
+    }
+};
+
+class Children : public School {
+public: 
+    void Forma() {
+        cout << "Sholnaya forma uchenikov.";
+    }
+};
+
+class Foto {
+public :
+    void Forma(School *School) {
+        School->Forma();
+    }
+
+};
+int main()
+{
+    Foto VVRLI;
+    Teacher SSV;
+    Children devyatuyB;
+
+    VVRLI.Forma(&SSV);
+    VVRLI.Forma(&devyatuyB);
+}
